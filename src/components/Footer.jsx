8611e52d1bc276a023453ba85a0ce2a440a1e90d
Ahmed <ahmedmahmoudkzm@gmail.com>
2023-07-22 00:04:50 +0300

    Finished styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,36 +6,38 @@ function Footer() {
     return (
         <footer>
             <div>
-                <div>
-                    <h1>Resumio</h1>
+                <div id='footer-items'>
+                    <div>
+                        <h1>Resumio</h1>
+                    </div>
+                    <div>
+                        <ul>
+                            <UlLink page={'Home'} isSelected={false} />
+                            <UlLink page={'Create CV'} isSelected={false} />
+                            <UlLink page={'Templates'} isSelected={false} />
+                            <UlLink page={'Support'} isSelected={false} />
+                            <UlLink page={'Terms'} isSelected={false} />
+                            <UlLink page={'About'} isSelected={false} />
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <ul>
-                        <UlLink page={'Home'} isSelected={false} />
-                        <UlLink page={'Create CV'} isSelected={false} />
-                        <UlLink page={'Templates'} isSelected={false} />
-                        <UlLink page={'Support'} isSelected={false} />
-                        <UlLink page={'Terms'} isSelected={false} />
-                        <UlLink page={'About'} isSelected={false} />
-                    </ul>
+                <div id='copyrights'>
+                    <p>
+                        <strong>Made by </strong>
+                        <a
+                            href="https://github.com/Ahmedmk11"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Ahmed Mahmoud.&#160;
+                        </a>
+                    </p>
+                    <p>
+                        Copyright ©<span id="starting-year"> 2023</span>
+                        <span id="current-year"></span>
+                        <span>. All rights reserved.</span>
+                    </p>
                 </div>
-            </div>
-            <div>
-                <p>
-                    <strong>Made by </strong>
-                    <a
-                        href="https://github.com/Ahmedmk11"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Ahmed Mahmoud
-                    </a>
-                </p>
-                <p>
-                    Copyright ©<span id="starting-year"> 2023</span>
-                    <span id="current-year"></span>
-                    <span>. All rights reserved.</span>
-                </p>
             </div>
         </footer>
     )
