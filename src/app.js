ff35b98fe23f08f616715aaba321397ff6c17304
Ahmed <ahmedmahmoudkzm@gmail.com>
2023-07-30 15:46:43 +0300

    Added server for sending mails

---
+++ src/app.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-undef */
+
+var express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+var nodemailer = require('nodemailer')
+var app = express()
+let port = process.env.PORT || 8080
+
+app.use(
+    cors({
+        origin: '*',
+    })
+)
+
+app.use(bodyParser.json())
+
+app.post('/', (req, res) => {
+    let messageTitle = req.body.curr.topic
+    let messageEmail = req.body.curr.email
+    let messageBody = req.body.curr.message
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'resumiomail@gmail.com',
+            pass: process.env.pass,
+        },
+    })
+
+    var mailOptions = {
+        from: 'resumiomail@gmail.com',
+        to: 'resumiomail@gmail.com',
+        subject: messageTitle,
+        html: `
+        <!DOCTYPE html>
+        <html lang="en" style="padding: 40px; width: 100%;">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Email</title>
+            </head>
+            <body style="color: #1e4b70; background-color: #f4f7f8 !important; padding: 20px; width: 95%; margin: auto;">
+                <h3>Email: ${messageEmail}</h3>
+                <p>${messageBody}</p>
+            </body>
+                <footer style="margin-top: 50px; width: 100%; height:fit-content ; display: flex; justify-content: center; align-items: center;">
+                    <p style="margin: 0%; font-weight: 600; font-size: medium;text-align: center;">
+                        Resumio © 2023 All Rights Reserved.
+                    </p>
+                </footer>
+            </body>
+        </html>
+        `,
+    }
+
+    transporter.sendMail(mailOptions, function (error) {
+        if (error) {
+            console.log(error)
+        }
+    })
+})
+
+app.listen(port, (err) => {
+    if (err) throw err
+    console.log(`server running on port ${port}`)
+})
--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -8,15 +8,32 @@ function Support() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 
+    const sendMail = async (t, e, m) => {
+        let msg = {
+            topic: t,
+            email: e,
+            message: m,
+        }
+        let msgStr = await JSON.stringify(msg)
+        let response = await fetch('', {
+            method: `POST`,
+            headers: { 'Content-Type': 'application/json' },
+            body: msgStr,
+        })
+        return response.body
+    }
+
     const submitSupport = () => {
-        const topic = document.getElementById('support-topic').value;
-        const message = document.getElementById('support-message').value;
-        if (topic === '' || message === '') {
-            alert('Please fill in all fields');
-        } else {
-            alert('Thank you for your message. We will get back to you shortly.');
+        const topic = document.getElementById('support-topic');
+        const email = document.getElementById('support-email');
+        const message = document.getElementById('support-message');
+        if (topic.reportValidity() && email.reportValidity() && message.reportValidity()) {
+            sendMail(topic.value, email.value, message.value);
+            document.getElementsByClassName('form')[0].submit();
             document.getElementById('support-topic').selectedIndex = 0;
+            document.getElementById('support-email').value = '';
             document.getElementById('support-message').value = '';
+            return
         }
     }
 
@@ -35,19 +52,28 @@ function Support() {
                     <p>Welcome to our support page. How can we help you?</p>
                     <form className='form'>
                         <label htmlFor="support-topic">Topic:</label>
-                        <select id="support-topic">
+                        <select id="support-topic" required>
                             <option value="" disabled selected>--Please choose an option--</option>
                             <option value="suggestion">Suggestion</option>
                             <option value="technical">Technical Problem</option>
                             <option value="general">General</option>
                         </select>
                         <br />
-                        <label htmlFor="support-message">Message:</label>
-                        <textarea id="support-message" placeholder='Your message here..'></textarea>
+                        <label htmlFor="support-email">Email</label>
+                        <input
+                            id="support-email"
+                            type="email"
+                            name="email"
+                            placeholder="johnsmith@email.com"
+                            required
+                        />
+                        <br />
+                        <label htmlFor="support-message">Message</label>
+                        <textarea id="support-message" placeholder='Your message here..' required></textarea>
                         <br />
                     </form>
                 </div>
-                <Button name="Send" click={submitSupport} />
+                <Button name="Send" isSubmit={true} click={submitSupport} />
             </div>
             <Footer />
         </>
